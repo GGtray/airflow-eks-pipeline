6bc09fc751d766cde3fe8e8bfbee08f23f60a481
Lizi Zhang <31552858+GGtray@users.noreply.github.com>
2020-06-11 11:49:29 -0500

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,41 +1,17 @@
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import BranchPythonOperator
-from datetime import datetime, timedelta
+from airflow.operators.python_operator import PythonOperator
 
-default_args = {
-    'owner': 'me',
-    'start_date': datetime(2019, 6, 27, 0, 0, 0),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1)
-}
+def print_hello():
+    return 'Hello world!'
 
+dag = DAG('hello_world', description='Simple tutorial DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
 
-def some_condition():
-    a = 1
-    if a > 2:
-        return 'second_pipeline'
-    return 'first_pipeline'
+dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
 
+hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
-dag = DAG(dag_id='branching',
-          default_args=default_args,
-          max_active_runs=4,
-          schedule_interval='0 * * * *',
-          catchup=False)
-
-start_task = DummyOperator(task_id='start_task', dag=dag)
-
-branch = BranchPythonOperator(task_id='validation', python_callable=some_condition, dag=dag)
-
-first_pipeline = DummyOperator(task_id='first_pipeline', dag=dag)
-
-second_pipeline = DummyOperator(task_id='second_pipeline', dag=dag)
-
-first_pipeline_next_step = DummyOperator(task_id='first_pipeline_next_step', dag=dag)
-second_pipeline_next_step = DummyOperator(task_id='second_pipeline_next_step', dag=dag)
-
-start_task >> branch
-branch >> first_pipeline >> first_pipeline_next_step
-branch >> second_pipeline >> second_pipeline_next_step
-
+dummy_operator >> hello_operator
