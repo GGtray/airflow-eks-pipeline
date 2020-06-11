400959c328f63a0c2a16b82335099b90b92ebd0d
Lizi Zhang <31552858+GGtray@users.noreply.github.com>
2020-06-11 14:29:57 -0500

    Update hello-world-3.py

--- hello-world-3.py
+++ hello-world-3.py
@@ -4,14 +4,14 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 def print_hello():
-    return 'Hello world!'
+    return 'Hello world-3!'
 
-dag = DAG('hello_world', description='Simple tutorial DAG',
-          schedule_interval='0 12 * * *',
+dag = DAG('hello_world-3', description='Simple tutorial DAG',
+          schedule_interval='0 16 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
+dummy_operator = DummyOperator(task_id='dummy_task-3', retries=3, dag=dag)
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+hello_operator = PythonOperator(task_id='hello_task-3', python_callable=print_hello, dag=dag)
 
 dummy_operator >> hello_operator
